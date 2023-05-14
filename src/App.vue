ca489b02acaf4a84965a7ad6194a015e1cd5e6d7
Rhaast <wanghaojie_1995@163.com>
2023-05-14 16:46:48 +0800

    定制主题色

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+<el-button type="primary">default</el-button>
+<el-button type="primary">Primary</el-button>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -14,12 +14,21 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({importStyle:'sass'})],
     }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        additionalData:`
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
     }
   }
 })
