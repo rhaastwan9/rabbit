4bf1b5419b7a8553d48d72bb017a9411b4b7e84b
Rhaast <wanghaojie_1995@163.com>
2023-05-14 20:59:05 +0800

    pinia优化重复请求

--- src/views/Layout/components/LayoutFixed.vue
+++ src/views/Layout/components/LayoutFixed.vue
@@ -1,7 +1,10 @@
 <script setup>
 // vueUse
 import { useScroll } from '@vueuse/core'
+import { useCategoryStore } from '@/stores/category';
+
 const { y } = useScroll(window)
+const categoryStore = useCategoryStore()
 </script>
 
 <template>
@@ -13,35 +16,10 @@ const { y } = useScroll(window)
         <li class="home">
           <RouterLink to="/">首页</RouterLink>
         </li>
-        <li>
-          <RouterLink to="/">居家</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">美食</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">服饰</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">母婴</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">个护</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">严选</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">数码</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">运动</RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/">杂项</RouterLink>
+        <li v-for="item in categoryStore.categoryList" :key="item.id">
+          <RouterLink to="/">{{item.name}}</RouterLink>
         </li>
       </ul>
-
       <div class="right">
         <RouterLink to="/">品牌</RouterLink>
         <RouterLink to="/">专题</RouterLink>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,11 @@ import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from './components/LayoutFixed.vue';
+import { onMounted } from 'vue';
+import { useCategoryStore } from '@/stores/category';
+
+const categoryStore = useCategoryStore()
+onMounted(()=>categoryStore.getCategory())
 </script>
 
 <template>
