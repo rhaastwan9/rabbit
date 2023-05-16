2a27996047fa756531dcadbbd608c4e984f8a05b
Rhaast <wanghaojie_1995@163.com>
2023-05-16 20:15:15 +0800

    home-面板组件封装，新鲜好物和人气推荐业务实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,11 +1,34 @@
+import  httpInstance  from '@/utils/http'
+
 /**
  * @description: 获取banner图
  * @param {*}
  * @return {*}
  */
-import  httpInstance  from '@/utils/http'
 export function getBannerAPI (){
   return httpInstance({
     url:'home/banner'
+  })
+}
+
+/**
+ * @description: 获取新鲜好物
+ * @param {*}
+ * @return {*}
+ */
+export const getNewAPI = () => {
+  return httpInstance({
+    url:'/home/new'
+  })
+}
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+  return  httpInstance({
+    url:'/home/hot'
   })
 }
