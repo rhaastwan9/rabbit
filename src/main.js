7809734b0afac0d06ad309644812bfbc1428cbfb
Rhaast <wanghaojie_1995@163.com>
2023-05-23 23:11:12 +0800

    使用pinia数据持久化插件

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
 
 import App from './App.vue'
 import router from './router'
@@ -7,8 +8,9 @@ import { lazyPlugin } from './directives'
 
 import './styles/common.scss'
 const app = createApp(App)
-
-app.use(createPinia())
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedState)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.mount('#app')
